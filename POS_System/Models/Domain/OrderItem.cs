57a7bae65acfd61c54111e18dde0e312d3818f7c
ThisaraRajapakshe <thisararajapakshe2020@gmail.com>
2025-11-27 12:13:43 +0530

    Add Order and OrderItem entities with migrations
    Introduces Order and OrderItem domain models, their DTOs, and updates the DbContext to support order management. Adds EF Core migration to create Orders and OrderItems tables with relationships, enabling order tracking and itemization in the POS system.

---
+++ POS_System/Models/Domain/OrderItem.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace POS_System.Models.Domain
+{
+    public class OrderItem
+    {
+        [Key]
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string OderId { get; set; }
+        public Order Order { get; set; }
+
+        public string ProductLineItemId { get; set; }
+        //SnapShot
+        public string ProductName { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal DisplayPrice { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal SalesPrice { get; set; }
+        public int Quantity { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal SubTotal { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Cost { get; set; }
+
+    }
+}
